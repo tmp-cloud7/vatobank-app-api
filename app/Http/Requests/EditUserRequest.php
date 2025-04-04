ccc4eecc0ac55ac6139ef91ea4334b45ff6aa39f
taio-ops <tayopopoola92@gmail.com>
2025-04-04 12:16:51 +0100

    Updated Requests and Authentication Controller

--- app/Http/Requests/EditUserRequest.php
+++ app/Http/Requests/EditUserRequest.php
@@ -21,41 +21,7 @@ class EditUserRequest extends FormRequest
      *
      * @return array<string, ValidationRule|array|string>
      */
-    // public function rules(): array
-    // {
-
-    //     return [
-    //         'name' => ['nullable', 'string', 'min:2', 'max:200'],
-    //         'lastname' => ['nullable', 'string', 'min:2', 'max:200'],
-    //         'middlename' => ['nullable', 'string', 'min:2', 'max:200'],
-    //         'email' => ['nullable', 'email', 'max:200', 'unique:users'],
-    //         'password' => ['nullable', 'string', 'min:8', 'max:200'],
-    //         'phone_number' => ['nullable', 'string', 'min:10', 'max:20', 'unique:users'],
-    //         'address' => ['nullable', 'string', 'max:200'],
-    //         'sog' => ['nullable', 'string', 'max:20'],
-    //         'dob' => ['nullable', 'date'], 
-    //         'gender' => ['nullable', 'string','in:male,female,other'],
-    //        'profile_image' => ['nullable', 'image', 'mimes:jpeg,png,jpg', 'max:2046'],
-
-    //     ];
-    // }
-//     public function rules()
-// {
-//     return [
-//         'name' => 'nullable|string|max:255',
-//         'lastname' => 'nullable|string|max:255',
-//         'middlename' => 'nullable|string|max:255',
-//         'email' => 'nullable|email|max:255|unique:users,email,'.auth()->id(),
-//         'password' => 'nullable|string|min:8|max:200',
-//         'phone_number' => 'nullable|string|max:20',
-//         'address' => 'nullable|string|max:255',
-//        'sog' => 'nullable|string|max:20',
-//         'dob' => 'nullable|date',
-//         'gender' => 'nullable|in:male,female,other',
-//         // 'profile_image' => 'nullable|image|mimes:jpeg,png,jpg|max:2048'
-//     ];
-    
-// }
+  
 public function rules()
 {
     return [
